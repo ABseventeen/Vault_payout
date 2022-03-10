52294d80a9cf3e72da20f29b8adcbdbb5edc9adc
ABseventeen <76699920+ABseventeen@users.noreply.github.com>
2022-03-10 16:51:12 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,18 +1,14 @@
 @csrf_exempt
 @decorators.api_view(["POST"])
 @decorators.permission_classes([permissions.AllowAny])
-def nach_debit_status(request):
+def nach_debit_status(request): 
     data = JSONParser().parse(request)
     try:
         if 'event' in data and 'payload' in data and 'nach_debit' in data["payload"]:
             transaction = Transaction.objects.get(digio_transaction_id=data["payload"]["nach_debit"]["transaction_id"])
             if data["event"] == "nach.debit.success":
                 transaction.debit_status = 2
-                # need to add 2 more status to txn - 1 after consolidated.settlement.completed, 1 after successful payout
-                # Initiate payout from Hypto
-                initiate_payout(request)
-
-                
+                # no payout here
             if data["event"] == "nach.debit.failed":
                 transaction.debit_status = 3 
                 transaction.failure_reason = data["payload"]["nach_debit"]["failure_description"]
@@ -23,6 +19,41 @@ def nach_debit_status(request):
     except ObjectDoesNotExist:
         return JsonResponse(data={"message": "Transaction not found"}, status=200)
 
+@csrf_exempt
+@decorators.api_view(["POST"])
+@decorators.permission_classes([permissions.AllowAny])
+def settlement_status(request):
+    data = JSONParser().parse(request)
+    try:
+        if 'event' in data and 'payload' in data and 'consolidated_settlement' in data["payload"]:
+            transaction = Transaction.objects.get(digio_transaction_id=data["payload"]["consolidated_settlement"]["transaction_id"])
+            if data["event"] == "consolidated.settlement.completed":
+                transaction.debit_status = 5
+                # Initiate payout from Hypto
+                initiate_payout(request) # Should we pay here instatly or wait for some time?
+                transaction.save()
+            return JsonResponse(data={"message": "Transaction amount received in our account"}, status=200)
+        else:
+            return JsonResponse(data={"message": "Not in proper format"}, status=200)
+    except ObjectDoesNotExist:
+        return JsonResponse(data={"message": "Transaction not found"}, status=200)
+
+@csrf_exempt
+@decorators.api_view(["POST"])
+@decorators.permission_classes([permissions.AllowAny])
+def payee_credit_status(request):
+    data = JSONParser().parse(request)
+    try:
+        if 'payload' in data:
+            transaction = Transaction.objects.get(digio_transaction_id=data["reference_number"])
+            if data['status'] == "COMPLETED":
+                transaction.debit_status = 6
+                transaction.save()
+            return JsonResponse(data={"message": "Transaction made to payee"}, status=200)
+        else:
+            return JsonResponse(data={"message": "Not in proper format"}, status=200)
+    except ObjectDoesNotExist:
+        return JsonResponse(data={"message": "Transaction not found"}, status=200)      
 
     
 @csrf_exempt
@@ -45,7 +76,7 @@ def initiate_payout(request):
                 # "udf1": "UDF Test 1",
                 # "udf2": "UDF Test 2",
                 # "udf3": "UDF Test 3",
-                "reference_number": "transaction.digio_transaction_id",
+                "reference_number": "transaction.digio_transaction_id", #is this schedule id or txn id (given by npci)
                 # "connected_banking": false
             }
         }
@@ -65,6 +96,4 @@ def initiate_payout(request):
         return JsonResponse(data={"message": "transfer success"}, status=200)  #where do we store the response object? how to show res data here?
     except ObjectDoesNotExist:
         return JsonResponse(data={"message": "Invalid Transaction Id or Network error"}, status=404)
-© 2022 GitHub, Inc.
-Terms
-Privacy
+
